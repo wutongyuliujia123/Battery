35f0fecc88bd91081fa6ea84ae6c1862778e9cc1
liujia.json <liujia.json@shandagames.com>
2018-12-31 15:53:17 +0800

    调整目录结构

--- app/src/main/java/com/battery/bms/MainActivity.java
+++ app/src/main/java/com/battery/bms/MainActivity.java
@@ -1,7 +1,9 @@
-package com.battery.battery;
+package com.battery.bms;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.battery.battery.R;
 
 public class MainActivity extends AppCompatActivity {
 
