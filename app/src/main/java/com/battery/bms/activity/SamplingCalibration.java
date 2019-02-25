4e698b6b8f880ae97f38722a5e97eaa7fd625ca7
liujia.json <liujia.json@shandagames.com>
2019-02-25 19:35:19 +0800

    增加界面

--- app/src/main/java/com/battery/bms/activity/SamplingCalibration.java
+++ app/src/main/java/com/battery/bms/activity/SamplingCalibration.java
@@ -2,13 +2,14 @@ package com.battery.bms.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 import com.battery.battery.R;
 
-public class ParameterRepair extends AppCompatActivity {
+public class SamplingCalibration extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parameter_repair);
+        setContentView(R.layout.activity_sampling_calibration);
     }
 }
